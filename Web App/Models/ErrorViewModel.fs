021a088a8d8f5883fde81a284afb0a27b0eb27df
victor <victorcasiano1693@gmail.com>
2022-05-17 20:32:37 -0700

    Add project files.

---
+++ Web App/Models/ErrorViewModel.fs
@@ -0,0 +1,8 @@
+namespace Web_App
+
+open System
+
+type ErrorViewModel private () =
+    member val RequestId : string = null with get, set
+
+    member val ShowRequestId : bool = true with get, set
